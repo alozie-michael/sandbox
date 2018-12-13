1c06a533ddfb71baae7e7a1f1c61eeb8c4d63c11
mujib ishola <mujib.ishola@gmail.com>
2018-12-13 15:50:42 +0100

    WIP MI UI

--- apicockpit/src/main/webapp/app/shared/shared.module.ts
+++ apicockpit/src/main/webapp/app/shared/shared.module.ts
@@ -4,13 +4,14 @@ import { NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
 import { NgbDateMomentAdapter } from './util/datepicker-adapter';
 import { ApicockpitSharedLibsModule, ApicockpitSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective } from './';
 import { NgSelectModule } from '@ng-select/ng-select';
+import { SidebarDropdownDirective } from 'app/shared/directives/sidebar-dropdown.directive';
 
 @NgModule({
     imports: [ApicockpitSharedLibsModule, ApicockpitSharedCommonModule, NgSelectModule],
-    declarations: [JhiLoginModalComponent, HasAnyAuthorityDirective],
+    declarations: [JhiLoginModalComponent, HasAnyAuthorityDirective, SidebarDropdownDirective],
     providers: [{ provide: NgbDateAdapter, useClass: NgbDateMomentAdapter }],
     entryComponents: [JhiLoginModalComponent],
-    exports: [ApicockpitSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective, NgSelectModule],
+    exports: [ApicockpitSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective, NgSelectModule, SidebarDropdownDirective],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ApicockpitSharedModule {}
